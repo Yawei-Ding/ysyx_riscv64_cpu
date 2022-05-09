1353e5984b111aaa2aacb6890939745553b87fe8
ysyx_22040561-Ding Yawei <dingyawei@stu.pku.edu.cn>
2022-05-09 19:46:18 +0800

    before starting pa1

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,29 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include <Vtop.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
+
+int main() 
+{
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+	reset(10);  // 复位10个周期
+    while(1) 
+    {
+        nvboard_update();
+        single_cycle();
+    }
 }
